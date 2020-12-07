6fcb7501db349359b6a9ab4ec58cb3a424a9eddb
tngusmiso <k1miso012@gmail.com>
2020-12-07 17:33:43 +0900

    Set View of MVC

--- PracticeDesignPattern/ViewController.swift
+++ PracticeDesignPattern/ViewController.swift
@@ -9,11 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var ageTextField: UITextField!
+    @IBOutlet weak var peopleTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
-
